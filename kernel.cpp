2e0eaf77f15e79db7f80c5f97860737f1ae26487
Darkrifts <darkrifts.cpp@gmail.com>
2016-09-09 19:52:40 -0500

    Fixed

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,3 @@
-// Build with i686-elf-g++ -c kernel.cpp -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-
 #include <stddef.h>
 #include <stdint.h>
 #include "tstdout.h"
@@ -12,10 +10,8 @@ static void kwait(uint64_t ticks)
 	for(uint64_t i = 0ul; i < ticks; i++) {asm("nop");}
 }
 
-extern "C" /* Use C linkage for kernel_main. */
+extern "C"
 void kernel_main(void) {
-	/* Initialize terminal interface */
 	terminal_initialize();
-
-	getString(1, c, true);
+	/* TODO: ADD STUFF WHEN IO AND MEMORY ACTUALLY WORKS */
 }
